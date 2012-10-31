aca5647bc10181ae5e79ab2b56a991910bc04f05
George Valdes <gv2186@gmail.com>
2012-10-31 02:08:25 -0400

    Edited and Added foreLayers

--- LayerManagement.js
+++ LayerManagement.js
@@ -12,6 +12,7 @@ Change other layers' lineweights according to name
 */
 
 var hiddenLayers = new Array();
+var foreLayers = new Array();
 var len = document.layers.length; // = 6
 var lastLayer = len - 1;
 print(lastLayer);
@@ -25,34 +26,40 @@ var hiddenStyle = {
 var foregroundStyle = {
 	strokeColor: '#00ff00',
 	fillColor: null,
-	strokeWidth: 1
+	strokeWidth: 5
 }
 
 //Search through layers and find Hidden
 //Add them to a new Array list which we will access later.
 for (var i = 0; i < len; i++) {
 	var str = document.layers[i].name;
-
 	print(str);
 		if (str.match(/HIDDEN/gi) == "HIDDEN"){
 			hiddenLayers.push(document.layers[i]);
 			}	
 			else {
 				print("Fuck "+ i);
-				var foreChildren = document.layers[i].children;
-				for (var j = 0; j < foreChildren.length; j++) {
-					foreChildren[j].style = foregroundStyle; 
-				};
-				
+				foreLayers.push(document.layers[i]);							
 		}
 };
 
+// Take Hidden Layers and assign their paths the (hiddenStyle)
 print(hiddenLayers);
 for (var i = 0; i < hiddenLayers.length; i++) {
 	var children = hiddenLayers[i].children;
+	
 	for (var j = 0; j < children.length; j++) {
 		children[j].style = hiddenStyle;
 	};
+
 	hiddenLayers[i].color = '#ff0000';
 	hiddenLayers[i].moveBelow(document.layers[lastLayer]);
+};
+
+// Take remaining Layers and assign their paths the (foregroundStyle)
+for (var i = 0; i < foreLayers.length; i++) {
+	var fChild = foreLayers[i].children;
+	for (var j = 0; j < fChild.length; j++) {
+		fChild[j].style = foregroundStyle;
+	};					 
 };
