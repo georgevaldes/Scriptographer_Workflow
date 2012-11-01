060c634fd26cdcceb41e7dfb29f3c424155664bc
George Valdes <gv2186@gmail.com>
2012-11-01 04:55:32 -0400

    Added UI to control lineweight and visibility of hidden lines

--- LayerManagement.js
+++ LayerManagement.js
@@ -11,6 +11,11 @@ Hide Layers (Optional)
 Change other layers' lineweights according to name
 */
 
+var values = {
+    lineweight: .5,
+    checkbox: true
+};
+
 //Global Variables
 var hiddenLayers = new Array();
 var foreLayers = new Array();
@@ -19,50 +24,81 @@ var lastLayer = len - 1;
 //print(lastLayer);
 
 
-//Path Styles
-var hiddenStyle = {
-	strokeColor: '#0040FF',
-    fillColor: null,
-    strokeWidth: .5
+// Define the interface components to edit each value. Note that they
+// need to use the same names as the properties in the values object:
+var components = { 
+    lineweight: {
+        type: 'number', label: 'Hidden Layer Lineweight',
+        range: [0, 1], steppers: true, units: 'point'
+    },
+    ruler1: {
+        type: 'ruler'
+    },
+       checkbox: {
+        type: 'boolean', label: 'Hidden Layers Visible?',
+    },
+    create: {
+        type: 'button', value: 'Dale!',
+        onClick: function() {
+
+        	//visibility
+        	var hiddenvis = values.checkbox;
+
+			//Path Styles
+			var foregroundStyle = {
+				strokeColor: '#000000',
+				fillColor: null,
+				strokeWidth: 2
+			};
+
+            var hiddenStyle = {
+				strokeColor: '#0040FF',
+			    fillColor: null,
+			    strokeWidth: values.lineweight
+			};
+            
+            //Search through layers and find Hidden
+			//Add them to a new Array list which we will access later.
+			for (var i = 0; i < len; i++) {
+				var str = document.layers[i].name;
+				print(str);
+					if (str.match(/HIDDEN/gi) == "HIDDEN"){
+						hiddenLayers.push(document.layers[i]);
+						}	
+						else {
+							foreLayers.push(document.layers[i]);							
+					}
+			}
+
+			// Take Hidden Layers and assign their paths the (hiddenStyle)
+			// print(hiddenLayers);
+			for (var i = 0; i < hiddenLayers.length; i++) {
+				var children = hiddenLayers[i].children;
+				
+				for (var j = 0; j < children.length; j++) {
+					children[j].style = hiddenStyle;
+				}
+
+				hiddenLayers[i].color = '#ff0000';
+				hiddenLayers[i].moveBelow(document.layers[lastLayer]);
+				hiddenLayers[i].visible = hiddenvis;
+			}
+
+			// Take remaining Layers and assign their paths the (foregroundStyle)
+			for (var i = 0; i < foreLayers.length; i++) {
+				var fChild = foreLayers[i].children;
+				for (var j = 0; j < fChild.length; j++) {
+					fChild[j].style = foregroundStyle;
+				}					 
+			}
+        }
+    }    
 };
 
-var foregroundStyle = {
-	strokeColor: '#000000',
-	fillColor: null,
-	strokeWidth: 2
-}
+// Now we create the palette window using the components
+// and values definitions:
+var palette = new Palette('Layer Management v.01', components, values);
 
-//Search through layers and find Hidden
-//Add them to a new Array list which we will access later.
-for (var i = 0; i < len; i++) {
-	var str = document.layers[i].name;
-	print(str);
-		if (str.match(/HIDDEN/gi) == "HIDDEN"){
-			hiddenLayers.push(document.layers[i]);
-			}	
-			else {
-				print("Fuck "+ i);
-				foreLayers.push(document.layers[i]);							
-		}
-};
 
-// Take Hidden Layers and assign their paths the (hiddenStyle)
-// print(hiddenLayers);
-for (var i = 0; i < hiddenLayers.length; i++) {
-	var children = hiddenLayers[i].children;
-	
-	for (var j = 0; j < children.length; j++) {
-		children[j].style = hiddenStyle;
-	};
 
-	hiddenLayers[i].color = '#ff0000';
-	hiddenLayers[i].moveBelow(document.layers[lastLayer]);
-};
 
-// Take remaining Layers and assign their paths the (foregroundStyle)
-for (var i = 0; i < foreLayers.length; i++) {
-	var fChild = foreLayers[i].children;
-	for (var j = 0; j < fChild.length; j++) {
-		fChild[j].style = foregroundStyle;
-	};					 
-};
